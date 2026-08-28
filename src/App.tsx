2206f383592937c593db130dc160038085be02a1
soheil hasanjani <soheilhasanjani@gmail.com>
2026-08-28 23:56:27 +0330

    Add theme provider and mode toggle components; update App component to include theme context and toggle functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "@/components/mode-toggle";
+
 function App() {
-    return <div>Real-Time Options</div>;
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div>Real-Time Options</div>
+      <ModeToggle />
+    </ThemeProvider>
+  );
 }
 
 export default App;
